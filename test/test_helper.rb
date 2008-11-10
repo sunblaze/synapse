0054496a69885791c27d6d8ddb674d3a641843b4
jsmith <sunblaze@macmini.(none)>
2008-11-10 00:31:10 -0500

    Add fixture_replacement to experiment with

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -33,6 +33,9 @@ class Test::Unit::TestCase
   # Note: You'll currently still have to declare fixtures explicitly in integration tests
   # -- they do not yet inherit this setting
   fixtures :all
+  
+  # Use FixtureReplacement2
+  include FixtureReplacement
 
   # Add more helper methods to be used by all tests here...
 	def assert_response_equals_file(file_name)
